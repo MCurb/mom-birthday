5d9c42aebb26d5d02598961d8d09654fa2c2a877
MCurb <marcoscurbeco03@gmail.com>
2025-12-17 01:36:45 -0600

    feat: add styling and some interactivity

--- javascript.js
+++ javascript.js
@@ -1,6 +1,6 @@
 const slidesOne = [...document.querySelectorAll(".carousel-one li")];
 const slidesTwo = [...document.querySelectorAll(".carousel-two li")];
-const slidesThree = [...document.querySelectorAll(".carousel-three li")]
+const slidesThree = [...document.querySelectorAll(".carousel-three li")];
 const carousels = [slidesOne, slidesTwo, slidesThree];
 let currentActiveIndex = 0;
 let intervalId;
@@ -15,18 +15,18 @@ intervalId = setInterval(() => {
 }, 1000);
 
 function nextSlide() {
-    for (const slide of slidesOne) {
-      if (slide.classList.contains("active-slide")) {
-        if (currentActiveIndex >= slidesOne.length - 1) {
-          currentActiveIndex = 0;
-          showSlide();
-          break;
-        }
-        currentActiveIndex++;
+  for (const slide of slidesOne) {
+    if (slide.classList.contains("active-slide")) {
+      if (currentActiveIndex >= slidesOne.length - 1) {
+        currentActiveIndex = 0;
         showSlide();
         break;
       }
+      currentActiveIndex++;
+      showSlide();
+      break;
     }
+  }
 }
 
 //Helper Functions
@@ -54,3 +54,15 @@ function removeActiveSlide() {
     });
   }
 }
+
+const surpriseBtn = document.querySelector(".surprise-btn");
+const video = document.querySelector("video");
+
+surpriseBtn.addEventListener("click", () => {
+  video.style.display = "block";
+  confetti({
+    particleCount: 80,
+    spread: 70,
+    origin: { y: 0.6 },
+  });
+});
